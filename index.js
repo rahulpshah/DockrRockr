9c421705749382ca5fb0570f2a22942f6f4f723c
Krunal Gala <kgala@ncsu.edu>
2016-10-16 13:40:38 -0400

    Initial commit: Starting bot on slack

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let Bot = require('./Bot');
+
+const bot = new Bot({
+  token: process.env.SLACK_TOKEN,
+  autoReconnect: true,
+  autoMark: true
+});
+
+bot.respondTo('hello', (message, channel, user) => {
+  bot.send(`Hello to you too, ${user.name}!`, channel)
+}, true);
