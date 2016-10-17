b942fe30a9c081e531eaa509a471fdef6e33b83b
Krunal Gala <kgala@ncsu.edu>
2016-10-17 13:09:13 -0400

    Creating and editing dockerfile and basic Redis implementation

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 var FSlack = require('node-slack-upload');
 var fs = require('fs');
+
 class Bot {
   constructor(opts) {
     let slackToken = opts.token;
@@ -65,51 +66,50 @@ class Bot {
   }
     respondTo(keywords, callback, start) 
     {
-  // If 'start' is truthy, prepend the '^' anchor to instruct the
-  // expression to look for matches at the beginning of the string
-        if (start) {
-            keywords = '^' + keywords;
-        }
+      // If 'start' is truthy, prepend the '^' anchor to instruct the
+      // expression to look for matches at the beginning of the string
+      if (start) {
+        keywords = '^' + keywords;
+      }
 
-  // Create a new regular expression, setting the case 
-  // insensitive (i) flag
-        let regex = new RegExp(keywords, 'i');
+      // Create a new regular expression, setting the case 
+      // insensitive (i) flag
+      let regex = new RegExp(keywords, 'i');
 
-  // Set the regular expression to be the key, with the callback
-  // function as the value
-        this.keywords.set(regex, callback);
+      // Set the regular expression to be the key, with the callback
+      // function as the value
+      this.keywords.set(regex, callback);
     }
 
     // Send a message to a channel, with an optional callback
     send(message, channel, cb) 
     {
-        this.slack.sendMessage(message, channel.id, () => {
+      this.slack.sendMessage(message, channel.id, () => {
         if (cb) {
-            cb();
+          cb();
         }
-        });
+      });
     }
 
     fileUpload(path, channel, cb) 
     {
-        this.fslack.uploadFile({
-    file: fs.createReadStream( path),
-    filetype: 'post',
-    title: 'Docker File',
-    initialComment: 'Here is your docker file!',
-    channels: channel.id
-}, function(err) {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        console.log('done');
-    }
-    if (cb) {
+      this.fslack.uploadFile({
+      file: fs.createReadStream( path),
+      filetype: 'post',
+      title: 'Docker File',
+      initialComment: 'Here is your docker file!',
+      channels: channel.id
+      }, function(err) {
+          if (err) {  
+            console.error(err);
+          }
+          else {
+            console.log('done');
+          }
+          if (cb) {
             cb();
-        }
-});
-
+          }
+        });
     }
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 'use strict';
+var fs = require('fs');
+const redis = require('redis');
+const client = redis.createClient();
 
 let Bot = require('./bot.js');
 
@@ -13,7 +16,67 @@ bot.respondTo('hello', (message, channel, user) => {
 }, true);
 
 bot.respondTo('create', (path, channel, user) => {
-  bot.fileUpload(`package.json`, channel,function(err,res){
+  bot.fileUpload(`package.json`, channel, function(err,res) {
   	bot.send('file uploaded',channel);
   })
 }, true);
+
+//Redis connection
+client.on('error', (err) => {
+    console.log('Error ' + err);
+});
+
+client.on('connect', () => {
+  console.log('Connected to Redis!');
+});
+
+//Storing in Redis
+bot.respondTo('store', (message, channel, user) => {
+  let msg = getArgs(message.text);
+  console.log(msg);
+  client.set(user.name, msg, (err) => {
+    if (err) {
+      bot.send('Oops! I tried to store that but something went wrong :(', channel);
+    } else {
+      bot.send(`Okay ${user.name}, I will remember that for you.`, channel);
+    }
+  });
+}, true);
+
+//Retrieve from Redis
+bot.respondTo('retrieve', (message, channel, user) => {
+  console.log(user.name)  
+  client.get(user.name, (err, reply) => {
+	  if (err) {
+	    console.log(err);
+	    return;
+	  }
+	  bot.send('Here\'s what I remember: ' + reply, channel);
+	  console.log(`Retrieved: ${reply}`);
+	});
+});
+
+// Take the message text and return the arguments
+function getArgs(msg) {
+  return msg.split(' ').slice(1);
+}
+
+
+//Create Dockerfile from template and replace with user credentials
+fs.createReadStream('Files/DockerFileTemplate').pipe(fs.createWriteStream('DockerFile'));
+var mapObj = {FullName:"Krunal Gala",Email:"krunal3103@gmail.com",AppName:"DockerRocker"};
+
+var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
+fs.readFile("DockerFile", 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var result = data.replace(re, function(matched){
+  return mapObj[matched];
+});
+
+  fs.writeFile("DockerFile", result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+});
+ 
