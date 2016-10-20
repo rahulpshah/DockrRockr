a31d91f8a101329bba89980b7cabbb52ad8f0bfe
samrudhisharma <ssharm17@.ncsu.edu>
2016-10-20 19:45:45 -0400

    Selenium Tests

--- Selenium/src/test/java/selenium/tests/WebTest.java
+++ Selenium/src/test/java/selenium/tests/WebTest.java
@@ -45,8 +45,8 @@ public class WebTest
 		WebElement pw = driver.findElement(By.id("password"));
 
 		// Type in our test user login info.
-		email.sendKeys("test@ncsu.edu");
-		pw.sendKeys("****");
+		email.sendKeys("ssharm17@ncsu.edu");
+		pw.sendKeys("Karuna215");
 
 		// Click
 		WebElement signin = driver.findElement(By.id("signin_btn"));
@@ -55,8 +55,8 @@ public class WebTest
 		// Wait until we go to general channel.
 		wait.until(ExpectedConditions.titleContains("general"));
 
-		driver.get("https://dockrrockr.slack.com/messages/testing");
-		wait.until(ExpectedConditions.titleContains("testing"));
+		driver.get("https://dockrrockr.slack.com/messages/tests");
+		wait.until(ExpectedConditions.titleContains("tests"));
 	}
 	
 	@AfterClass
@@ -66,6 +66,8 @@ public class WebTest
 		driver.quit();
 	}
 	
+	
+	//Tests for Use Case 1:
 	@Test
 	public void checkHelloConversation()
 	{
@@ -77,73 +79,101 @@ public class WebTest
 		
 		wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
 		
-		WebElement getIDHello = driver.findElement(
+		ArrayList<WebElement> getIDHello = (ArrayList<WebElement>) driver.findElements(
 				By.xpath("//span[@class='message_body' and text() = 'hello']/../.."));
 
+		wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
 		
-		String idHello = getIDHello.getAttribute("id").split("_")[2];	
+		String idHello = getIDHello.get(getIDHello.size()-1).getAttribute("id").split("_")[2];	
+		System.out.println(idHello);
 	    int index = Integer.valueOf(idHello);
+	    System.out.println(index);
 		
 		wait.withTimeout(10, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
 		
-	    WebElement getIDHelloResponseContent = driver.findElement(
+		WebElement getIDHelloResponseContent = driver.findElement(
 				By.xpath("//ts-message[contains(@id,'"+(index+1)+"')]/div/span[@class='message_body']"));
 	    
-	    WebElement getIDHelloResponseContentName = driver.findElement(
+		wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		
+		WebElement getIDHelloResponseContentName = driver.findElement(
 				By.xpath("//ts-message[contains(@id,'"+(index+1)+"')]/div/span[@class='message_body']/span[@class='mention']"));
 		
 		
 		assertEquals(getIDHelloResponseContent.getText(), "Hi, "+getIDHelloResponseContentName.getText()+"! What can I do for you today?");
 		
+		wait.withTimeout(10, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		
 	}
+	
 	
 	@Test
 	public void checkCreateDockerConversation()
 	{
 		
-		//Testing for request response below:
+		
+		//Happy Path for Use Case 1:
 		WebElement messageBot = driver.findElement(By.id("message-input"));
-		messageBot.sendKeys("Create a Docker");
+		messageBot.sendKeys("Create a Docker file");
 		messageBot.sendKeys(Keys.RETURN);
 		
 		wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
 		
-		WebElement getIDHello = driver.findElement(
-				By.xpath("//span[@class='message_body' and text() = 'Create a Docker']/../.."));
+		ArrayList<WebElement> getIDHello = (ArrayList<WebElement>) driver.findElements(
+				By.xpath("//span[@class='message_body' and text() = 'Create a Docker file']/../.."));
 
+		wait.withTimeout(30, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
 		
-	    String idHello = getIDHello.getAttribute("id").split("_")[2];	
-	    int index = Integer.valueOf(idHello);
+		String idHello = getIDHello.get(getIDHello.size()-1).getAttribute("id").split("_")[2];	
+		int index = Integer.valueOf(idHello);
 	        
-	    wait.withTimeout(10, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		wait.withTimeout(10, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
 		
 		WebElement getIDHelloResponseContent = driver.findElement(
 				By.xpath("//ts-message[contains(@id,'"+(index+1)+"')]/div/span[@class='message_body']"));
-			
-		//System.out.println(getIDHelloResponseContent.getText().split("\\r?\\n")[0]);
+		
+		wait.withTimeout(10, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		
+		//System.out.println(getIDHelloResponseContent.getText().split("\\r?\\n")[1].split("#")[0]);
 		assertEquals(getIDHelloResponseContent.getText().split("\\r?\\n")[0], ("Please fill this form to create a dockerfile"));
-		assertEquals(getIDHelloResponseContent.getText().split("\\r?\\n")[1], ("http://localhost:8081/"));
+		
+		assertEquals(getIDHelloResponseContent.getText().split("\\r?\\n")[1].split("#")[0],("http://localhost:8081"));
+		
+		wait.withTimeout(10, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		//Show interaction steps for form here, fall created response etc.
+		
+		//Fail Path for Use Case 1:
 		
 	}
 	
+	//TODO
+	//Test For Use Case 2: 
+	@Test
+	public void checkNotifyConversation() {
+		assertTrue(true);		
+	}
+	
+	
+	//Test for Use Case 3:
 	@Test
 	public void checkCreateDeployConversation()
 	{
 		
+		//Happy Path for Use Case 3:
 		//Testing for request response below:
 		WebElement messageBot = driver.findElement(By.id("message-input"));
 		messageBot.sendKeys("deploy");
 		messageBot.sendKeys(Keys.RETURN);
 		
-	    wait.withTimeout(10, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		wait.withTimeout(30, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
 		
-		WebElement getIDHello = driver.findElement(
+		ArrayList<WebElement> getIDHello = (ArrayList<WebElement>) driver.findElements(
 				By.xpath("//span[@class='message_body' and text() = 'deploy']/../.."));
 		
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.MINUTES);
 		
-		String idHello = getIDHello.getAttribute("id").split("_")[2];	
-	    int index = Integer.valueOf(idHello);
+		String idHello = getIDHello.get(getIDHello.size()-1).getAttribute("id").split("_")[2];	
+		int index = Integer.valueOf(idHello);
 	    
 		WebElement getIDHelloResponseContent = driver.findElement(
 				By.xpath("//ts-message[contains(@id,'"+(index+1)+"')]/div/span[@class='message_body']"));
@@ -152,6 +182,11 @@ public class WebTest
 		//System.out.println(getIDHelloResponseContent.getText());
 		assertEquals(getIDHelloResponseContent.getText().split("\\r?\\n")[0], "Your app has been deployed at http://amazonaws.com/mock-url");
 		
+		wait.withTimeout(10, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		
+		//Fail Path for Use Case 3:
+		
 	}
+
 
 }
