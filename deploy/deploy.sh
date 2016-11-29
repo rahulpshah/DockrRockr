c6af25af5f848acdeac069c424710f74b67ab695
Rahul P Shah <rshah5@ncsu.edu>
2016-11-29 02:40:40 -0500

    Added deploy.yml and few other scripts

---
+++ deploy/deploy.sh
@@ -0,0 +1,15 @@
+# Prerequisites for running this code
+# Ansible installed on the this machine
+# npm, node, python installed
+# ENV TOKENS - AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_KEY_PATH
+
+# Install all the package dependencies
+npm install
+
+# Script to spin up ec2 instance
+node aws.js rahul /Users/Rahul/Downloads/rahul.pem
+
+# This will create an invntory file which will be used by ansible to perform the configuration mgmt.
+# The inventory will contain the ip address of ec2 instance, the user of the instance and private key for the following instance
+# This playbook deploys our code with all the dependencies
+ansible-playbook -i inventory setup_ami.yml
--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,7 +1,12 @@
+npm install
+node aws.js
+
 export SLACK_TOKEN=$1
 
 # remote instance is AMI
 ansible-playbook -i inventory setup_ami.yml
-# remote instance is centos
+# remote instance is Centos
 ansible-playbook -i inventory setup.yml
 
+# Based heavily on the Ansible documentation on EC2:
+# http://docs.ansible.com/ec2_module.html
